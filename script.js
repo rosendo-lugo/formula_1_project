97d318d72d43c41040ed4c4792e9d319dbd3e656
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-12-14 20:22:11 -0600

    first update to script

--- script.js
+++ script.js
@@ -1,54 +1,100 @@
-function createBarChart(data) {
-    // Set dimensions and margins for the chart
-    const margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 600 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+// Pseudocode for joining and processing data
+function processData(driverStandings, drivers, results) {
+    // Convert driverStandings and results to a map for faster lookup
+    const standingsMap = new Map(driverStandings.map(d => [d.driverId, d]));
+    const resultsMap = new Map(results.map(r => [r.driverId, r]));
 
-    // Append SVG object to the chart div
-    const svg = d3.select("#chart")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+    // Join drivers with standings and results
+    const joinedData = drivers.map(driver => {
+        const standing = standingsMap.get(driver.driverId) || {};
+        const result = resultsMap.get(driver.driverId) || {};
 
-    // X axis
-    const x = d3.scaleBand()
-        .range([0, width])
-        .domain(data.map(d => d.category)) // Replace 'category' with the relevant column name
-        .padding(0.2);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.someNumericValue)]) // Use the numeric attribute
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // Bars
-    svg.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", d => x(d.category)) // Replace 'category' with the relevant column name
-        .attr("y", d => y(d.someNumericValue))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d.someNumericValue))
-        .attr("fill", "#69b3a2");
-}
-
-// Loading and parsing the data from driver_standings.csv
-d3.csv("driver_standings.csv").then(function(data) {
-    // Parse and clean your data here
-    // For example, converting strings to numbers
-    data.forEach(d => {
-        d.points = +d.points; // Assuming 'points' is a column in your CSV
+        return {
+            ...driver,
+            ...standing,
+            ...result
+        };
     });
 
-    // Call createBarChart with the parsed data
-    createBarChart(data);
+    // Aggregate total wins for each driver
+    const winsByDriver = joinedData.reduce((acc, driver) => {
+        const wins = parseInt(driver.wins) || 0;
+        acc[driver.driverId] = (acc[driver.driverId] || 0) + wins;
+        return acc;
+    }, {});
+
+    // Convert the aggregated data to an array suitable for D3
+    const processedData = Object.keys(winsByDriver).map(driverId => {
+        return {
+            driverId: driverId,
+            totalWins: winsByDriver[driverId],
+            driverName: drivers.find(d => d.driverId === driverId)?.name
+        };
+    });
+
+    return processedData;
+}
+
+
+// Pseudocode for creating a bar chart
+function createBarChart(data) {
+// Set dimensions and margins for the chart
+    const margin = { top: 20, right: 20, bottom: 70, left: 40 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// Append SVG object to the chart div
+const svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Set the ranges for the scales
+const x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.1);
+const y = d3.scaleLinear()
+    .range([height, 0]);
+
+// Scale the range of the data in the domains
+x.domain(data.map(d => d.driverName));
+y.domain([0, d3.max(data, d => d.totalWins)]);
+
+// Append the rectangles for the bar chart
+svg.selectAll(".bar")
+  .data(data)
+  .enter().append("rect")
+  .attr("class", "bar")
+  .attr("x", d => x(d.driverName))
+  .attr("width", x.bandwidth())
+  .attr("y", d => y(d.totalWins))
+  .attr("height", d => height - y(d.totalWins))
+  .attr("fill", "#69b3a2");
+
+// Add the x-axis
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .style("text-anchor", "end")
+  .attr("dx", "-.8em")
+  .attr("dy", ".15em")
+  .attr("transform", "rotate(-65)");
+
+// Add the y-axis
+svg.append("g")
+  .call(d3.axisLeft(y));
+}
+
+
+// Load data using d3.csv and process it
+d3.csv("driver_standings.csv").then(driverStandings => {
+    d3.csv("drivers.csv").then(drivers => {
+        d3.csv("results.csv").then(results => {
+            const processedData = processData(driverStandings, drivers, results);
+            createBarChart(processedData);
+        });
+    });
 });

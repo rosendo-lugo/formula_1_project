153c798c5d6db610432511721b8112d3eb033cc7
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-12-17 12:31:10 -0600

    sync

--- my_chart.js
+++ my_chart.js
@@ -1,5 +1,5 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
   type: 'bar',
   data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -26,10 +26,6 @@ var myChart = new Chart(ctx, {
       }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+      responsive: true,
     }
   });

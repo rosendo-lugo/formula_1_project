86bd86666cbb0203c69301636280655c4dd309bc
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-12-17 18:22:36 -0600

    sync

--- my_chart_extra.js
+++ my_chart_extra.js
@@ -49,3 +49,58 @@ function updateChart(chart, type, dataSet, canvas) {
 // Initialize charts for each canvas with the appropriate dataset selector
 initializeChart('myChart', '.dataSetSelector1');
 initializeChart('earning', '.dataSetSelector2');
+
+
+function parseCSV(csvData) {
+    const lines = csvData.split('\n');
+    return lines.map(line => line.split(','));
+}
+function updateRecentOrders(data) {
+    const tableBody = document.querySelector('#ordersTable tbody');
+    tableBody.innerHTML = ''; // Clear existing rows
+
+    data.forEach((row, index) => {
+        if (index === 0) return; // Assuming first row is headers
+
+        const tr = document.createElement('tr');
+        row.forEach(cell => {
+            const td = document.createElement('td');
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+}
+fetch('path/to/yourfile.csv')
+    .then(response => response.text())
+    .then(csvText => {
+        const [headers, ...rows] = parseCSV(csvText);
+        updateTableHeaders(headers);
+        updateTableRows(rows);
+    })
+    .catch(error => console.error('Error fetching CSV:', error));
+
+function parseCSV(csvData) {
+    const lines = csvData.split('\n');
+    return lines.map(line => line.split(','));
+}
+
+function updateTableHeaders(headers) {
+    const thead = document.querySelector('#ordersTable thead');
+    thead.innerHTML = ''; // Clear existing headers
+    const tr = document.createElement('tr');
+
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        tr.appendChild(th);
+    });
+
+    thead.appendChild(tr);
+}
+
+function updateTableRows(rows) {
+    // Similar to previous examples, create and append rows to tbody
+    // ...
+}

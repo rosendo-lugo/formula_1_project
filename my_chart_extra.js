3f7ea01471a85d06ef89cd4e3605b649543cd146
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-12-20 13:47:21 -0600

    sync

--- my_chart_extra.js
+++ my_chart_extra.js
@@ -70,36 +70,42 @@ function getChartConfig(chartType) {
                     responsive: true,
                 }
             };
-        case 'bar':
-            return {
-                labels = Utils.months({count: 7});
-                data = {
-                    labels: labels,
-                    datasets: [{
-                        label: 'My First Dataset',
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 205, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(201, 203, 207, 0.2)'
-                        ],
-                        borderColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                        'rgb(255, 205, 86)',
-                        'rgb(75, 192, 192)',
-                        'rgb(54, 162, 235)',
-                        'rgb(153, 102, 255)',
-                        'rgb(201, 203, 207)'
-                        ],
-                        borderWidth: 1
-                    }]
-                }
-            };
+            case 'bar':
+                return {
+                    data: {
+                        labels: Utils.months({ count: 7 }), // Make sure Utils.months is defined
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(255, 205, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(201, 203, 207, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgb(255, 99, 132)',
+                                'rgb(255, 159, 64)',
+                                'rgb(255, 205, 86)',
+                                'rgb(75, 192, 192)',
+                                'rgb(54, 162, 235)',
+                                'rgb(153, 102, 255)',
+                                'rgb(201, 203, 207)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                };
         case 'bubble':
             return {
                 data: {
@@ -126,9 +132,8 @@ function getChartConfig(chartType) {
             };
         case 'line':
             return {
-                labels = Utils.months({count: 7});
-                data = {
-                    labels: labels,
+                data: {
+                    labels: Utils.months({ count: 7 }), // Make sure Utils.months is defined
                     datasets: [{
                         label: 'My First Dataset',
                         data: [65, 59, 80, 81, 56, 55, 40],
@@ -136,7 +141,8 @@ function getChartConfig(chartType) {
                         borderColor: 'rgb(75, 192, 192)',
                         tension: 0.1
                     }]
-                }
+                },
+                options: {}
             };
         case 'radar':
             return {
